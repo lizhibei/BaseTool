544452c7f7adc16f1c62402e42577e93977348be
dengjinwen <djw@xmgasy.com>
2018-07-19 11:18:38 +0800

    新建项目

---
+++ library/src/main/java/com/dengjinwen/basetool/library/ScreenUitl.java
@@ -0,0 +1,38 @@
+package com.dengjinwen.basetool.library;
+
+import android.content.Context;
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.WindowManager;
+
+public class ScreenUitl {
+
+    /**
+     * 获取屏幕宽度
+     * @param context
+     * @return
+     */
+    public static int getScreenWidth(Context context) {
+        WindowManager windowmanager = (WindowManager) context
+                .getSystemService(Context.WINDOW_SERVICE);
+        Display display = windowmanager.getDefaultDisplay();
+        DisplayMetrics outMetrics = new DisplayMetrics();
+        display.getMetrics(outMetrics);
+        return outMetrics.widthPixels;
+    }
+
+    /**
+     * 得到屏幕高度
+     * @param context
+     * @return
+     */
+    public static int getScreenHeight(Context context) {
+        WindowManager manager = (WindowManager) context
+                .getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics dm = new DisplayMetrics();
+        manager.getDefaultDisplay().getMetrics(dm);
+        int height = dm.heightPixels;
+
+        return height;
+    }
+}
