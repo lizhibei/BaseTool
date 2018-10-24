b1cc94f3b70e126792a1394482fd8c22d1565e6e
dengjinwen <djw@xmgasy.com>
2018-10-24 10:16:26 +0800

    在ScreenUtil添加获取状态栏高度和虚拟键盘高度

--- library/src/main/java/com/dengjinwen/basetool/library/tool/ScreenUitl.java
+++ library/src/main/java/com/dengjinwen/basetool/library/tool/ScreenUitl.java
@@ -5,6 +5,8 @@ import android.util.DisplayMetrics;
 import android.view.Display;
 import android.view.WindowManager;
 
+import java.lang.reflect.Method;
+
 public class ScreenUitl {
 
     /**
@@ -34,5 +36,44 @@ public class ScreenUitl {
         int height = dm.heightPixels;
 
         return height;
+    }
+
+    /**
+     * 获得状态栏高度
+     * @param context
+     * @return
+     */
+    private int getStatusBarHeight(Context context){
+        int result=0;
+        int resourceId=context.getResources().getIdentifier("status_bar_height",
+                "dimen","android");
+        if(resourceId>0){
+            result =context.getResources().getDimensionPixelSize(resourceId);
+        }
+        return result;
+    }
+
+
+    /**
+     * 获取虚拟导航键的高度
+     * @param context
+     * @return
+     */
+    public static int getVirtualBarHeight(Context context) {
+        int vh = 0;
+        WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        Display display = windowManager.getDefaultDisplay();
+        DisplayMetrics dm = new DisplayMetrics();
+        try {
+            @SuppressWarnings("rawtypes")
+            Class c = Class.forName("android.view.Display");
+            @SuppressWarnings("unchecked")
+            Method method = c.getMethod("getRealMetrics", DisplayMetrics.class);
+            method.invoke(display, dm);
+            vh = dm.heightPixels - display.getHeight();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return vh;
     }
 }
