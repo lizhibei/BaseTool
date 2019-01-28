c957b469df3579d613bbcf3e2ffd701730c356a0
dengjinwen <djw@xmgasy.com>
2019-01-28 16:52:36 +0800

    修改状态栏的获取

--- library/src/main/java/com/dengjinwen/basetool/library/tool/ScreenUitl.java
+++ library/src/main/java/com/dengjinwen/basetool/library/tool/ScreenUitl.java
@@ -65,13 +65,17 @@ public class ScreenUitl {
      * @return
      */
     public static int getStatusBarHeight(Context context){
-        int result=0;
-        int resourceId=context.getResources().getIdentifier("status_bar_height",
-                "dimen","android");
-        if(resourceId>0){
-            result =context.getResources().getDimensionPixelSize(resourceId);
+        int statusBarHeight = -1;
+        try {
+            Class<?> clazz = Class.forName("com.android.internal.R$dimen");
+            Object object = clazz.newInstance();
+            int height = Integer.parseInt(clazz.getField("status_bar_height")
+                    .get(object).toString());
+            statusBarHeight = context.getResources().getDimensionPixelSize(height);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        return result;
+        return statusBarHeight;
     }
 
 
