88373a1961ff71d3759354843d97a175f659e35b
dengjinwen <djw@xmgasy.com>
2019-01-28 17:17:50 +0800

    修复bug

--- app/src/main/java/com/dengjinwen/basetool/MainActivity.java
+++ app/src/main/java/com/dengjinwen/basetool/MainActivity.java
@@ -15,7 +15,7 @@ import com.dengjinwen.basetool.activity.SelectImageAndActivity;
 import com.dengjinwen.basetool.activity.selfview.SelfViewActivity;
 import com.dengjinwen.basetool.adapter.MainAdapter;
 import com.dengjinwen.basetool.entity.MainItem;
-import com.dengjinwen.basetool.library.function.screenAdaptation.ScreenAdapterTools;
+import com.dengjinwen.basetool.library.tool.ScreenUitl;
 import com.dengjinwen.basetool.util.FilePicker;
 
 import java.util.ArrayList;
@@ -34,7 +34,9 @@ public class MainActivity extends BaseActivity  {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ScreenAdapterTools.getInstance().loadView(getWindow().getDecorView());
+//        ScreenAdapterTools.getInstance().loadView(getWindow().getDecorView());
+        ScreenUitl.setHead(this,findViewById(R.id.head_top), R.color.head_translucent,
+                ScreenUitl.StatusBarStyle.COLOR);
         mContext=this;
         initData();
         initview();
--- library/src/main/java/com/dengjinwen/basetool/library/tool/ScreenUitl.java
+++ library/src/main/java/com/dengjinwen/basetool/library/tool/ScreenUitl.java
@@ -2,6 +2,7 @@ package com.dengjinwen.basetool.library.tool;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Build;
 import android.util.DisplayMetrics;
 import android.view.Display;
@@ -121,7 +122,7 @@ public class ScreenUitl {
             int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                     | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
             decorView.setSystemUiVisibility(option);
-            context.getWindow().setStatusBarColor(context.getResources().getColor(color));
+            context.getWindow().setStatusBarColor(Color.TRANSPARENT);
             if (style == StatusBarStyle.COLOR) {
                 LinearLayout.LayoutParams linearParams = (LinearLayout.LayoutParams) view.getLayoutParams();
                 linearParams.height = getStatusBarHeight(context);
