9683adb9a3f7cb6d17102f76f917ad61efa6db91
dengjinwen <djw@xmgasy.com>
2019-06-24 19:07:15 +0800

    修改设置头部背景

--- library/src/main/java/com/dengjinwen/basetool/library/function/BaseToolActivity.java
+++ library/src/main/java/com/dengjinwen/basetool/library/function/BaseToolActivity.java
@@ -8,7 +8,6 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
-import com.dengjinwen.basetool.library.R;
 import com.dengjinwen.basetool.library.function.dialog.product.LoadDialog;
 import com.dengjinwen.basetool.library.function.mvp.IBaseView;
 import com.dengjinwen.basetool.library.tool.ScreenUitl;
@@ -17,14 +16,14 @@ public class BaseToolActivity extends AppCompatActivity implements IBaseView {
 
     private LoadDialog loadDialog;
 
-    public void setTextStatusBar(){
-        ScreenUitl.setHead(this,findViewById(R.id.head_top),
+    public void setTextStatusBar(int emptyID){
+        ScreenUitl.setHead(this,findViewById(emptyID),
                 android.R.color.white,ScreenUitl.StatusBarStyle.COLOR);
         ScreenUitl.setStatusBarLightMode(this);
     }
 
-    public void setImageStatusBar(){
-        ScreenUitl.setHead(this, findViewById(R.id.head_top),
+    public void setImageStatusBar(int emptyID){
+        ScreenUitl.setHead(this, findViewById(emptyID),
                 android.R.color.transparent, ScreenUitl.StatusBarStyle.IMAGE);
         ScreenUitl.setStatusBarDarkMode(this);
     }
